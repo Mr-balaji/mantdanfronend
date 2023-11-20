036a1a39bfbf61ae1ece46e63bdf895cf4b9ea1a
SuJit <86299830+sujityadav89@users.noreply.github.com>
2023-11-20 23:45:38 +0530

    changes

--- src/pages/sign-in.js
+++ src/pages/sign-in.js
@@ -1,4 +1,5 @@
 import { Link, json, useNavigate } from "react-router-dom";
+// import logo from '../../public/GRAMSEVA.png';
 import {
   Card,
   CardHeader,
@@ -65,37 +66,38 @@ export function SignIn() {
 <div class="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
     <div class="sm:mx-auto sm:w-full sm:max-w-md">
         <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            Sign in to your account
+           Login
         </h2>
+        {/* <Image src={logo}/> */}
    
     </div>
 
-    <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+    <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-xl">
         <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-            <form class="space-y-6" action="#" method="POST">
-                <div>
-                    <label for="email" class="block text-sm font-medium text-gray-700">
+            <form class="" action="#" method="POST">
+                <div class="mb-4">
+                    <label for="email" class="block text-lg font-semibold text-gray-700">
                     युजरचे नाव
                     </label>
-                    <div class="mt-1">
+                    <div class="">
                         <input id="email" name="email" onChange={(e)=>setUserName(e.target.value)} value={userName} type="email" autocomplete="email" required
-                            class="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                            class="appearance-none rounded-md relative block w-full p-5 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 text-lg"
                             placeholder="Enter your email address" />
                     </div>
                 </div>
 
                 <div>
-                    <label for="password" class="block text-sm font-medium text-gray-700">
+                    <label for="password" class="block text-lg font-semibold text-gray-700">
                     पासवर्ड
                     </label>
-                    <div class="mt-1">
+                    <div class="">
                         <input id="password" name="password" onChange={(e)=>setPassword(e.target.value)} value={password} type="password" autocomplete="current-password" required
-                            class="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                            class="appearance-none rounded-md relative block w-full p-5 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 text-lg"
                             placeholder="Enter your password" />
                     </div>
                 </div>
 
-                <div class="flex items-center justify-between">
+                {/* <div class="flex items-center justify-between">
                     <div class="flex items-center">
                         <input id="remember_me" name="remember_me" type="checkbox"
                             class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded" />
@@ -109,17 +111,17 @@ export function SignIn() {
                             Forgot your password?
                         </a>
                     </div>
-                </div>
+                </div> */}
 
-                <div>
+                <div className="mt-10">
                     <button type="submit" onClick={handleSubmit}
-                        class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                        class="group relative w-full flex justify-center items-center p-5 border border-transparent text-xl font-bold rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                       
-                        Sign in
+                       प्रवेश करा
                     </button>
                 </div>
             </form>
-            <div class="mt-6">
+            {/* <div class="mt-6">
 
                 <div class="relative">
                     <div class="absolute inset-0 flex items-center">
@@ -155,7 +157,7 @@ export function SignIn() {
                         </a>
                     </div>
                 </div>
-            </div>
+            </div> */}
         </div>
     </div>
 </div>
